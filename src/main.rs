b6bbfeb42a5829b4299b0be241a099b504d7c7e0
Simon Gasse <sgasse@users.noreply.github.com>
2022-12-01 20:54:21 +0100

    Size as you want it

--- src/main.rs
+++ src/main.rs
@@ -7,38 +7,50 @@ use yew::prelude::*;
 #[function_component(App)]
 fn app() -> Html {
     // Width and height states + callbacks
-    let width_txt = use_state(|| "4".to_owned());
-    let height_txt = use_state(|| "3".to_owned());
+    let width_state = use_state(|| 4usize);
+    let height_state = use_state(|| 3usize);
 
-    // let width_val = use_state(|| match width_txt.parse::<usize>() {
-    //     Ok(width) => width,
-    //     _ => 3,
-    // });
-
-    let (width, height) = match (width_txt.parse::<usize>(), height_txt.parse::<usize>()) {
-        (Ok(width), Ok(height)) => (width, height),
-        _ => (3, 3),
-    };
-    let fields = use_state(|| initialize_fields(width, height));
+    let fields = use_state(|| initialize_fields(*width_state, *height_state));
 
     let on_width_change = {
-        let width_txt = width_txt.clone();
+        let width_state = width_state.clone();
+        let height_state = height_state.clone();
         let fields = fields.clone();
         Callback::from(move |input_event: InputEvent| {
             if let Some(value) = input_event.data() {
                 info!("Received width value {:?}", value);
-                info!("Fields {:?}", *fields);
-                width_txt.set(value);
+                if let Ok(width) = value.parse::<usize>() {
+                    info!("Parsed width value {:?}", width);
+                    width_state.set(width);
+                    fields.set(initialize_fields(width, *height_state));
+                }
+            }
+        })
+    };
+
+    let on_height_change = {
+        let width_state = width_state.clone();
+        let height_state = height_state.clone();
+        let fields = fields.clone();
+        Callback::from(move |input_event: InputEvent| {
+            if let Some(value) = input_event.data() {
+                info!("Received height value {:?}", value);
+                if let Ok(height) = value.parse::<usize>() {
+                    info!("Parsed height value {:?}", height);
+                    height_state.set(height);
+                    fields.set(initialize_fields(*width_state, height));
+                }
             }
         })
     };
 
     let on_field_click = {
+        let width_state = width_state.clone();
+        let height_state = height_state.clone();
         let fields = fields.clone();
-        // let width_val = width_val.clone();
         Callback::from(move |clicked_idx: usize| {
             info!("Clicked on field with index {}", clicked_idx);
-            let updated_fields = trigger_field(&fields, width, height, clicked_idx);
+            let updated_fields = trigger_field(&fields, *width_state, *height_state, clicked_idx);
             fields.set(updated_fields);
         })
     };
@@ -47,13 +59,13 @@ fn app() -> Html {
         <>
             <h1>{ "Shift Puzzle" }</h1>
             <input type="text" value="bla" />
-            <input type="text" value={(&*width_txt).clone()} oninput={on_width_change.clone()}/>
-            <input type="text" value={(&*height_txt).clone()} />
+            <input type="text" value={format!("{}", *width_state)} oninput={on_width_change.clone()}/>
+            <input type="text" value={format!("{}", *height_state)} oninput={on_height_change.clone()}/>
             <PuzzleBoard
                 fields={(&*fields).clone()}
                 on_click={on_field_click.clone()}
-                width={(width) as usize}
-                height={height as usize}
+                width={*width_state}
+                height={*height_state}
                 field_size={5}
                 field_unit={"rem"}
                 background_url={"https://scr.wfcdn.de/21565/Imgur-Memes-des-Jahrzehnts-1579171161-0-0.jpg"}
